7973cef7cf4ead846f2e5065baed5ac2d4080d3e
unknown <noemi.nyaradi@gmail.com>
2020-10-12 20:45:39 +0300

    Laborator1

---
+++ Lab01/src/main/java/ro/utcluj/ssatr/lab01/Exercitiul1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.utcluj.ssatr.lab01;
+
+/**
+ *
+ * @author Noemi
+ */
+public class Exercitiul1 {
+    
+    public static void main(String[] args) {
+        System.out.print("Hello Again");
+    }
+    
+}
